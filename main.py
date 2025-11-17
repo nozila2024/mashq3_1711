a69a67e5d1b4bcfcb73054cde65b1953ecae7cf9
nozila2024 <nozilarajabboyeva@gmail.com>
2025-11-17 16:09:39 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+#1-misol
+b = "mnopqrstuv"
+toza = b[:7]
+print(toza)
+
+#2-misol
+b = "examplestring"
+toza = b[1:8:6]+b[-4::3]
+print(toza)
+
+#3-misol
+b = "123456789abcdef"
+toza = b[1::2]
+print(toza)
+
+#4-misol
+b = "university"
+toza = b[3:7]
+print(toza)
+
+#5-misol
+b = "banana"
+toza = b[-2:]
+print(toza)
